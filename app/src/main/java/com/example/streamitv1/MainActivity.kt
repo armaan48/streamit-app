671b76a61e77cd4341b54c96e7c9017c98d5d968
dhruv-6 <dhruv6.sharma6@gmail.com>
2024-02-07 19:38:09 +0530

    some responsiveness changes

--- app/src/main/java/com/example/streamitv1/MainActivity.kt
+++ app/src/main/java/com/example/streamitv1/MainActivity.kt
@@ -32,7 +32,7 @@ class MainActivity : ComponentActivity() {
             StreamitTheme {
                 NavHost(
                     navController = navController,
-                    startDestination = "Main" ,
+                    startDestination = "Login" ,
                     route = "LoginSection"
                 ) {
                     composable(
